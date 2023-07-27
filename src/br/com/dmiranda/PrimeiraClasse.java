ef02f30434bd931d39d8220fff44e2a2699d46cd
198552 <dossantos_davidson@hotmail.com>
2023-07-27 01:15:45 -0300

    meu primeiro commit

---
+++ src/br/com/dmiranda/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.dmiranda;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.print("HalloWord");
+	}
+
+}
